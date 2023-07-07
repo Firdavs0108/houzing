6e39f6c2933b46fa7bbd6830071866e2ee090758
Firdavs Nazarov <fredrickuzb55@gmail.com>
2023-07-07 21:39:40 +0900

    Houzing

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,44 +1,50 @@
 import React, {useRef} from 'react'
-import { Container,Arrow } from './style'
+import { Container,Arrow, Img, Blur, Content } from './style'
 import { Carousel } from 'antd';
 
+import img1 from '../../assets/img/home.png'
+import img2 from '../../assets/img/home2.png'
 
-const contentStyle = {
-    margin: 0,
-    height: '160px',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-  };
-  
 
-const GenCarousel = () => {
-    const leftRef = useRef()
-    const rightRef = useRef()
+    const GenCarousel = () => {
+    const slider = useRef()
+
+
+
     const onChange = (currentSlide) => {
         console.log(currentSlide);
       };
+
+      const onMove =({
+        target:{
+            dataset: {name},
+        },
+        }) => {
+        if(name === 'right') slider.current.next()
+        if(name === 'left') slider.current.prev()
+
+      };
+
+
+
   return (
     <Container>
-        <Arrow left/>
-            <Carousel afterChange={onChange}>
-      <div>
-        <h3 style={contentStyle}>1</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>2</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>3</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>4</h3>
-      </div>
+            <Carousel ref={slider} afterChange={onChange}>
+  
+              <Img src={img1}/>
+              <Img src={img2}/>
             </Carousel>
-            <Arrow/>
+            <Blur/>
+            <Content>
+              <Content.Title>Skyper Pool Partment</Content.Title>
+              <Content.Desc>112 Glenwood Ave Hyde Park, Boston, MA</Content.Desc>
+              <Content.Price>$5,250 / month</Content.Price>
+            </Content>
+            <Arrow data-name='left' onClick={onMove}  left/>
+            <Arrow data-name='right' onClick={onMove} name='right' />
     </Container>
   )
 }
+
 
 export default GenCarousel
--- src/components/Carousel/style.js
+++ src/components/Carousel/style.js
@@ -5,7 +5,7 @@ import {ReactComponent as arrow} from '../../assets/icons/left_arrow.svg'
 const Container = styled.div`
     position: relative;
     height: 571px;
-    border: 1px solid red;
+
 `
 
 const Arrow = styled(arrow)`
@@ -14,20 +14,78 @@ const Arrow = styled(arrow)`
     width: 50px;
     height: 50px;
     padding: 18px;
-    background: rgba(0, 0, 0, 0.4);
+    background: rgba(225, 225, 225, 0.2);
     border-radius: 50%;
     transform: ${({left}) => (left ? 'rotate(180deg)'  : 'rotate(-90)')};
     left:  ${({left}) => !left && '20px'};
     right:  ${({left}) => left && '20px'};
     cursor: pointer;
      :hover{
-        opacity: 0.6;
+        background: rgba(225, 225, 225, 0.4);
     }
-
-  
 `;
 
+const Img = styled.img`
+    width: 100%;
+    height: 573px;
+    
+`
+
+const Blur = styled.div`
+    position: absolute;
+    top:0;
+    bottom:0;
+    right:0;
+    left:0;
+    background: rgba(0,0,0,0.4);
+ 
+`
+
+const Content = styled.div`
+    position: absolute;
+    top:0;
+    bottom:0;
+    right:0;
+    left:0;
+
+    display: flex;
+    flex-direction: column;
+    color: white;
+    align-items: center;
+    justify-content: center;
+   
+`
+
+Content.Title = styled.h1`
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 700;
+    font-style: 44px;
+    line-height: 48px;
+    letter-spacing: -0.02em;
+    color: white;
+`
+
+Content.Desc = styled.div`
+    color: #FFF;
+    font-family: 'Montserrat';
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px;
+`
+
+Content.Price = styled.div`
+    color: #FFF;
+    font-family: 'Montserrat';
+    font-size: 28px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 36px;
+    letter-spacing: -0.56px;
+`
 
 
 
-export{Container,Arrow,}
+
+export{Container,Arrow, Img, Blur, Content}
